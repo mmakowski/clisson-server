97132524c0278bb4a9acf5a187014a15be37e267
Maciek Makowski <maciek.makowski@gmail.com>
2012-03-18 20:44:09 +0000

    log4j appender test works

--- src/test/scala/integration/log4jAppenderTest.scala
+++ src/test/scala/integration/log4jAppenderTest.scala
@@ -1,6 +1,10 @@
 package integration
 
 import com.bimbr.clisson.server.config.Config
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.utils.URIUtils
+import org.apache.http.util.EntityUtils
 
 object log4jAppenderTest {
   val ConfigFile = "integration/log4jAppender/clisson-server.properties"
@@ -36,7 +40,10 @@ object log4jAppenderTest {
   }
 
   def checkTrail() = {
-    println("TODO: check trail")
+    val client = new DefaultHttpClient
+    val request = new HttpGet(URIUtils.createURI("http", "localhost", 9000, "/trail/msg-1", "", null))
+    val response = client.execute(request)
+    println("response to " + request.getURI + ": " + response + "\n" + EntityUtils.toString(response.getEntity))
   }
   
   def stopServer(server: Thread): Unit = {
@@ -63,13 +70,21 @@ object log4jAppenderTest {
     PropertyConfigurator.configure(getClass.getClassLoader.getResource("integration/log4jAppender/log4j.properties")) 
     val log = Logger.getLogger(getClass)
     
-    def run() = log.info("msg-1 is being processed")
-
+    def run() = {
+      log.info("awaiting msg-1 (this line should not be sent)")
+      log.info("msg-1: is being processed")
+      log.warn("this should be ignored as well")
+      log.error("msg-1: error happened!")
+    }
+    
     // the transformation from log4j Event to Clisson Event is a part of the client app
     class Transformation extends EventTransformation {
       def perform(log4jEvent: LoggingEvent): Event = {
         val logMessage = log4jEvent.getRenderedMessage
-        val msgId = "dupa"
+        val msgId = """(.+): .*""".r.findFirstMatchIn(logMessage) match {
+          case None    => throw new EventTransformation.IgnoreEventException
+          case Some(m) => m.group(1)
+        }
         new Event("log4jAppenderTest", new java.util.Date, Set(msgId), Set(msgId), logMessage)
       }
     }
