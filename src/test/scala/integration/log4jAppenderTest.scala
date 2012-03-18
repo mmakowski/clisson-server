ed126f137a51d5ea788e32e5ebac3bb9b8ccc05f
Maciek Makowski <maciek.makowski@gmail.com>
2012-03-18 20:07:54 +0000

    the app in log4j appender test sends an event to the server

--- src/test/scala/integration/log4jAppenderTest.scala
+++ src/test/scala/integration/log4jAppenderTest.scala
@@ -1,37 +1,78 @@
 package integration
 
+import com.bimbr.clisson.server.config.Config
+
 object log4jAppenderTest {
+  val ConfigFile = "integration/log4jAppender/clisson-server.properties"
+  System.setProperty("config", ConfigFile)
+    
   def main(args: Array[String]) = {
-    // TODO: delete database if exists
+    deleteDatabase()
     val server = startServer(args)
-    runLoggingApp()
+    loggingApp.run()
+    Thread.sleep(1000)
     checkTrail()
     stopServer(server) 
   }
   
+  // assumes usage of H2 database
+  def deleteDatabase(): Unit = {
+    val dbBase = Config.fromPropertiesFile(ConfigFile) match {
+      case Left(msg)  => sys.error(msg) 
+      case Right(cfg) => cfg("clisson.db.path").get
+    }
+    deleteIfExists(dbBase + ".h2.db")
+    deleteIfExists(dbBase + ".trace.db")
+  }
+  
   // TODO: separate JVM would be more realistic
-  def startServer(args: Array[String]) = {
+  def startServer(args: Array[String]): Thread = {
     val server = new Thread(new Runnable() {
-      def run() = {
-        System.setProperty("config", "log4jAppenderTest-server.properties")
-        com.bimbr.clisson.server.ClissonServerApp.main(args)
-      }
+      def run() = com.bimbr.clisson.server.ClissonServerApp.main(args)
     })
     server.start()
     Thread.sleep(5000)
     server
   }
 
-  def runLoggingApp() = {
-    println("TODO: run logging app")
-  }
-  
   def checkTrail() = {
     println("TODO: check trail")
   }
   
-  def stopServer(server: Thread) = {
+  def stopServer(server: Thread): Unit = {
     // server.stop() doesn't suffice, need more drastic solution
     System.exit(0)
   }
+  
+  def deleteIfExists(path: String): Unit = {
+    val file = new java.io.File(path)
+    if (file.exists) {
+      if (!file.delete()) sys.error("unable to delete " + file)
+    }
+  }
+  
+  // the app that logs through log4j and whose events should end up with the server
+  object loggingApp {
+    import scala.collection.JavaConversions._
+    import org.apache.log4j.{ Logger, PropertyConfigurator }
+    import org.apache.log4j.spi.LoggingEvent
+    import com.bimbr.clisson.client.log4j.EventTransformation
+    import com.bimbr.clisson.protocol.Event
+    
+    System.setProperty("clisson.config", "classpath://integration/log4jAppender/clisson-client.properties")
+    PropertyConfigurator.configure(getClass.getClassLoader.getResource("integration/log4jAppender/log4j.properties")) 
+    val log = Logger.getLogger(getClass)
+    
+    def run() = log.info("msg-1 is being processed")
+
+    // the transformation from log4j Event to Clisson Event is a part of the client app
+    class Transformation extends EventTransformation {
+      def perform(log4jEvent: LoggingEvent): Event = {
+        val logMessage = log4jEvent.getRenderedMessage
+        val msgId = "dupa"
+        new Event("log4jAppenderTest", new java.util.Date, Set(msgId), Set(msgId), logMessage)
+      }
+    }
+  }
+  
 }
