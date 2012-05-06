18b65c3bb1fb2b9e57e0f98a0545178ac83f4899
Maciek Makowski <maciek.makowski@gmail.com>
2012-05-06 23:01:39 +0100

    #6: building distributable zip is now part of SBT build

---
+++ project/build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+import com.github.retronym.SbtOneJar.oneJar
+
+object ClissonServerBuild extends Build {
+  val include = SettingKey[Seq[String]]("include-in-zip", "Files to include in the distributable zip, in addition to one-jar jar")
+  val zip = TaskKey[File]("zip", "Builds the distributable zip file")
+  
+  val zipSettings = Seq(
+    zip <<= (target, name, version, oneJar, include in zip, streams) map { 
+      (buildTarget, projectName, projectVersion, oneJarFile, extraFiles, s) =>
+      val dirName = projectName + "-" + projectVersion
+      val output = new java.io.File(buildTarget, dirName + ".zip")
+      val entries = (extraFiles.map(file) :+ oneJarFile) map (path => (path, dirName + "/" + path.getName))
+      s.log.info("Packaging " + output + " ...")
+      IO.zip(entries, output)
+      s.log.info("Done packaging.")
+      output
+    }
+  )
+  
+  lazy val root = Project(id       = "clisson-server",
+                          base     = file("."),
+                          settings = Project.defaultSettings ++ zipSettings)
+}
