e8d941a2f73cc19d561475ce337c8fe7301b062e
Maciek Makowski <maciek.makowski@gmail.com>
2012-05-07 00:29:12 +0100

    #6: publishing is now done as a part of SBT build (shell out to scp for now)

--- project/build.scala
+++ project/build.scala
@@ -3,12 +3,11 @@ import Keys._
 import com.github.retronym.SbtOneJar.oneJar
 
 object ClissonServerBuild extends Build {
+  // building distributable zip
   val filesToInclude = SettingKey[Seq[String]]("files-to-include", "Files to include in the distributable zip, in addition to one-jar jar")
   val zip = TaskKey[File]("zip", "Builds the distributable zip file")
-  
   val zipSettings = Seq(
-    zip <<= (target, name, version, oneJar, filesToInclude in zip, streams) map { 
-      (buildTarget, projectName, projectVersion, oneJarFile, extraFiles, s) =>
+    zip <<= (target, name, version, oneJar, filesToInclude in zip, streams) map { (buildTarget, projectName, projectVersion, oneJarFile, extraFiles, s) =>
       val dirName = projectName + "-" + projectVersion
       val output = new java.io.File(buildTarget, dirName + ".zip")
       val entries = (extraFiles.map(file) :+ oneJarFile) map (path => (path, dirName + "/" + path.getName))
@@ -19,7 +18,17 @@ object ClissonServerBuild extends Build {
     }
   )
   
+  // deploying to distribution site
+  val publishSettings = Seq(
+    publish <<= (zip) map { zipFile =>
+      // probably can be made more portable by reusing existing publish and specifying:
+      // publishTo := Some(Resolver.ssh("mmakowski", "mmakowski.webfactional.com", "~/webapps/bimbr/downloads"))
+      // -- just need a way to tell publish to send the zip instead of jar
+      Process(Seq("scp", zipFile.getAbsolutePath, "mmakowski@mmakowski.webfactional.com:~/webapps/bimbr/downloads/" + zipFile.getName)) !
+    }
+  )
+  
   lazy val root = Project(id       = "clisson-server",
                           base     = file("."),
-                          settings = Project.defaultSettings ++ zipSettings)
+                          settings = Project.defaultSettings ++ zipSettings ++ publishSettings)
 }
